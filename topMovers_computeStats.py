74adfa9b9ba29b42a229bc85a6a14859aaf25a56
doomed51 <doomed51@gmail.com>
2018-05-21 21:50:05 -0400

    1. added tickerReturnSinceFirst to compute avgPctChange, totalrecords, and date stats per ticker; 2. Addde printDataframeToExcel function

--- topMovers_computeStats.py
+++ topMovers_computeStats.py
@@ -1,9 +1,11 @@
 import numpy as np
 import pandas as pd
-
 import matplotlib.pyplot as plt
-from scipy import stats
 
+import dateutil
+import datetime
+
+from scipy import stats
 from sklearn.metrics import r2_score
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import scale
@@ -12,14 +14,38 @@ topMoversData = pd.read_csv("topMovers.csv", skipinitialspace = True)
 
 # cleanse the columns of any extra initial or trailing spaces
 topMoversData.columns = topMoversData.columns.str.strip()
-print()
+
+#normalize dates
+topMoversData['Date'] = topMoversData['Date'].apply(dateutil.parser.parse, dayfirst=False)
 
 # Define train data as the first 80% of the dataset 
-topMoversData_train = topMoversData[:int(len(topMoversData)*.8)]
+topMoversData_train = topMoversData#[:int(len(topMoversData)*.8)]
 
 #Define test data as the last 80% of the dataset 
 topMoversData_test = topMoversData[int(len(topMoversData)*.8):]
 
+def tickerReturnSinceFirst():
+    tickerCounts = topMoversData_train.groupby('Symbol').filter(lambda x: x['Symbol'].count() > 1)
+    
+    #tickerCounts = topMoversData_train['Symbol'].value_counts(),np.mean(topMoversData_train['% Change'])
+    #print(tickerCounts['Date'].count())
+    aggregators = {
+        '% Change' : {
+            'avgPctChange' : 'mean',
+            'totalRecords' : 'count'
+        },
+        'Date' : {
+            'firstDate' : 'min',
+            'daysSinceFirst' : lambda y: datetime.datetime.now().date() - min(y).date(),
+            'daysBetweenFirstAndLast' : lambda y: max(y).date() - min(y).date()
+        }        
+    }
+
+    agTickers = tickerCounts.groupby('Symbol').agg(aggregators)
+    print(agTickers)
+    agTickers.columns = agTickers.columns.droplevel(level=0)
+    return agTickers
+    
 def plotRankAndPctChange():
     #   Polynomial Regression
     rankPctChangePolynomial = np.poly1d(np.polyfit(topMoversData_train['Rank'], topMoversData_train['% Change'],20))
@@ -54,32 +80,17 @@ def  clusterPctChange():
     plt.figure(figsize=(8,6))
     plt.scatter(pctChangeArray[:,0], pctChangeArray[:,1], c=pctChangeKModel.labels_.astype(float))
     
+def writeDataframeToExcel(myDF, myFileName, mySheetName):
+    writer = pd.ExcelWriter(myFileName, engine='xlsxwriter')
+    myDF.to_excel(writer, sheet_name=mySheetName)
+    writer.save()
 
-# INCOMPLETE
-# run linear reg on ticker counts and display chart results
-def tickerCountLinReg():
-    # create a list of counts for each symbol
-    #ticker_counts = (topMoversData_train['Symbol'].value_counts().keys(), topMoversData_train['Symbol'].value_counts())
-    #ticker_counts = topMoversData_train.groupby("Symbol").filter(lambda g: g.Symbol.size > 20)
-    ticker_counts = topMoversData_train.groupby('Symbol')#.filter(lambda g: len(g) > 1)
-    
-    #p4 = np.poly1d(np.polyfit(topMoversData_train['Rank'], topMoversData_train['% Change'],15))
-    histogramValues = np.histogram(ticker_counts.size()) 
-    stats.expon.fit(histogramValues)
-    #print(ticker_counts['% Change'].agg([np.size, np.mean, np.std]))
-    
-    #slope, intercept, r_value, p_value, std_err = stats.linregress(np.histogram(ticker_counts.size()))
+writeDataframeToExcel(tickerReturnSinceFirst(), 'tickerStats.xlsx', 'Ticker Days')
+#plotRankAndPctChange()
+#clusterPctChange()
 
-    # print_chart to a list of tickers that appear more than once
-    #print_chart = ticker_counts[ticker_counts > 1]
-
-    #plt.hist(ticker_counts.size().tail(),100,range=(2,ticker_counts.max()))
-    plt.hist(ticker_counts.size(),30,range=(1,50))
-    #plt.scatter(np.histogram(ticker_counts.size())[0],np.histogram(ticker_counts.size())[1])
-    #print(ticker_counts.size().filter(lambda g: g.size() > 10))
-    plt.show()
-
-print(topMoversData_train.columns)
+# DRAW the chart on screen
+plt.show()
 
 
 #TODO
@@ -134,11 +145,5 @@ print(topMoversData_train.columns)
 
 # PRINT CHART TO FILE
 # plt.savefig('c:\\workbench\\Python-test\\MyPlot.png', format='png')
-
-#plotRankAndPctChange()
-clusterPctChange()
-
-# DRAW the chart on screen
-plt.show()
 
 # End chart output section #####################################
